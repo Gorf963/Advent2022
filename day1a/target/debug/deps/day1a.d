2f646b8d9bcb0b6faa1336e92eb17528cf5579e6
James Wiegand <James.Wiegand@verizon.net>
2022-12-18 17:27:08 -0500

    Delete day1a/target directory

--- day1a/target/debug/deps/day1a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jwiegand.MOBILEHEALTH\rust\Advent2022\day1a\target\debug\deps\day1a.exe: src\main.rs
-
-C:\Users\jwiegand.MOBILEHEALTH\rust\Advent2022\day1a\target\debug\deps\day1a.d: src\main.rs
-
-src\main.rs:
